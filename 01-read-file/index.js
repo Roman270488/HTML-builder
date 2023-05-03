eaed6f20eedff193f45c72e030e6d1e240ee8518
unknown <romanchernyshkov88@gmail.com>
2023-05-03 23:07:28 +0300

    fix: fixed the code in task 1

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs'); // подключаеи файлавую систему
 
 fs.readFile('01-read-file/text.txt', 'utf-8', (err, data) => {
+  if (err) throw err;
   console.log(data); // либо data.toString() без 'utf-8'
 });
