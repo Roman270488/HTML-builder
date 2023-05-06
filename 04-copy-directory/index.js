105850e4f91544f4fee59d0f826bcf4a43871e70
unknown <romanchernyshkov88@gmail.com>
2023-05-06 21:07:42 +0300

    feat: task 4 completed

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const defFolder = '04-copy-directory/files';
+const copyFolder = '04-copy-directory/files-copy';
+const copyDir = (files, filesCopy) => {
+  fs.mkdir(filesCopy, { recursive : true }, (err) => {
+    if (err) throw err;
+  
+    fs.readdir(filesCopy, { withFileTypes : true }, (err, data) => {
+      if (err) throw err;
+  
+      fs.readdir(files, { withFileTypes : true }, (err, data) => {
+        if (err) throw err;
+        data.forEach((elem) => {
+          fs.copyFile(`${files}/${elem.name}`, `${filesCopy}/${elem.name}`, () => {});
+        });
+      });
+
+      data.forEach((elem) => {fs.unlink(`${filesCopy}/${elem.name}`, () => {});});
+  
+    });
+
+  });
+}
+copyDir(defFolder, copyFolder);
+
+
 
