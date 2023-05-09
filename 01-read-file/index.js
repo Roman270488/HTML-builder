1dcf52c3740413de5d64b00b6ffdb7d3b0970e02
unknown <romanchernyshkov88@gmail.com>
2023-05-09 10:14:35 +0300

    fix: used readstrim

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,5 @@
 const fs = require('fs'); // подключаеи файлавую систему
 
-fs.readFile('01-read-file/text.txt', 'utf-8', (err, data) => {
-  if (err) throw err;
-  console.log(data); // либо data.toString() без 'utf-8'
+fs.createReadStream('01-read-file/text.txt').addListener('data', (data) => {
+  console.log(data.toString())
 });
